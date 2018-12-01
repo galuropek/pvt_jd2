1f751d6ea15967861f98ab72eeb0b51b6224fa14
Галушка Игорь <galuropek@gmail.com>
2018-12-01 11:09:30 +0300

    HibernateUtil: getInstance, getSession

---
+++ helloHibernate/src/main/java/by/pvt/util/HibernateUtil.java
@@ -0,0 +1,42 @@
+package by.pvt.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    //каждый экземлпяр threadLocal созается для одного потока
+    // (поток может обратиться только к одному threadLocal и больше ни к какому другоу)
+    // важно помнить!!!
+    ThreadLocal<Session> threadLocal = new ThreadLocal<>();
+
+    private SessionFactory sessionFactory;
+
+    private static HibernateUtil hibernateUtil;
+
+    private HibernateUtil() {
+        sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    public static HibernateUtil getInstance() {
+        if (hibernateUtil == null)
+            hibernateUtil = new HibernateUtil();
+        return hibernateUtil;
+    }
+
+    public Session getSession(String databaseName) {
+        return getSession();
+    }
+
+    public Session getSession() {
+        Session session = threadLocal.get();
+        if (session == null) {
+            session = sessionFactory.openSession();
+            threadLocal.set(session);
+        }
+        return session;
+    }
+}

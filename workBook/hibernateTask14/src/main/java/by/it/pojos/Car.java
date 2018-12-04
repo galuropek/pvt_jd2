f3861825f5be8ffe94d96d0011cc9260a27686bd
Галушка Игорь <galuropek@gmail.com>
2018-12-04 15:06:07 +0300

    create edit car

--- workBook/hibernateTask14/src/main/java/by/it/pojos/Car.java
+++ workBook/hibernateTask14/src/main/java/by/it/pojos/Car.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 @Entity
 public class Car implements Serializable {
 
+    private static final long serialVersionUID = -4391652778167283801L;
     @Id
     @GeneratedValue
     private Integer id;
@@ -16,5 +17,29 @@ public class Car implements Serializable {
     private String Mark;
     @Column
     private String Model;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getMark() {
+        return Mark;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setMark(String mark) {
+        Mark = mark;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
 
 }

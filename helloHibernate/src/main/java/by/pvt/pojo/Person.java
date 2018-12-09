da0a503006c72f6e8b3e128b349559de874d7a1b
Галушка Игорь <galuropek@gmail.com>
2018-12-09 23:27:35 +0300

    HELLOHIBERNATE

--- helloHibernate/src/main/java/by/pvt/pojo/Person.java
+++ helloHibernate/src/main/java/by/pvt/pojo/Person.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Person implements Serializable {
 
@@ -18,6 +19,16 @@ public class Person implements Serializable {
     private Date dateOfBirth;
 
     private Address address;
+
+    private List<String> titles;
+
+    public List<String> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(List<String> titles) {
+        this.titles = titles;
+    }
 
     public String getId() {
         return id;

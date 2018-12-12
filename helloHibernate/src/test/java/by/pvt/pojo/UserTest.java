352aee4fca6d922ebdb0997a3d87257a2cc8b126
Галушка Игорь <galuropek@gmail.com>
2018-12-12 22:26:26 +0300

    hellohibern

---
+++ helloHibernate/src/test/java/by/pvt/pojo/UserTest.java
@@ -0,0 +1,61 @@
+package by.pvt.pojo;
+
+import by.pvt.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    Session session;
+
+    @Before
+    public void setUp() throws Exception {
+        session = HibernateUtil.getInstance().getSession();
+    }
+
+    @Test
+    public void createIstance() {
+
+        User user = new User();
+        user.setUserLogin("qwerty");
+        user.setUserEmail("qwerty@mail.com");
+
+        UserDetails userDetails = new UserDetails();
+        userDetails.setPassword("12345678");
+        userDetails.setLoginAttempts(3);
+        userDetails.setExpiratedDate(new Timestamp(System.currentTimeMillis()));
+
+        user.setUserDetails(userDetails);
+        userDetails.setUser(user);
+
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(user);
+            session.saveOrUpdate(userDetails);
+            session.getTransaction().commit();
+            assertTrue(user.getId() > 0);
+            assertTrue(userDetails.getId() > 0);
+            assertEquals(user.getId(), userDetails.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (session != null && session.isOpen()) {
+            session.close();
+            session = null;
+        }
+    }
+}

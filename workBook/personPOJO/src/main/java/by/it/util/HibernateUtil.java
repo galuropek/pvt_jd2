28bc046277ad9fefad6bee92bcee9a9f3774363a
Галушка Игорь <galuropek@gmail.com>
2018-12-13 15:38:03 +0300

    edit

--- workBook/personPOJO/src/main/java/by/it/util/HibernateUtil.java
+++ workBook/personPOJO/src/main/java/by/it/util/HibernateUtil.java
@@ -1,10 +1,33 @@
 package by.it.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
 public class HibernateUtil {
+
+    private SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+        try {
+            Configuration configuration = new Configuration().configure();
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
 
     private static final EntityManagerFactory emFactory;
 

146d58d579e9608d2878229ab6ee785de78fa3f0
Галушка Игорь <galuropek@gmail.com>
2018-12-22 11:52:15 +0300

    Department work

--- helloHibernateAnnotations/src/main/java/by/pvt/pojo/Department.java
+++ helloHibernateAnnotations/src/main/java/by/pvt/pojo/Department.java
@@ -1,12 +1,21 @@
 package by.pvt.pojo;
 
+import org.hibernate.annotations.CollectionType;
+
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
     private String departmentName;
 
+    @OneToMany(mappedBy = "department", cascade = CascadeType.PERSIST)
     private Set<Employee> employees;
 
     public long getId() {

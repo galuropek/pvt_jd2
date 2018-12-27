75e089a05e78967e11428408271c6944c9ea4140
Галушка Игорь <galuropek@gmail.com>
2018-12-27 14:00:24 +0300

    add annotations

--- project2/src/main/java/io/swagger/model/AgreementRef.java
+++ project2/src/main/java/io/swagger/model/AgreementRef.java
@@ -5,7 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 
@@ -16,14 +22,20 @@ import javax.validation.constraints.*;
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-12-22T19:45:02.882+03:00")
 
+@Entity
 public class AgreementRef   {
   @JsonProperty("href")
+  @Column
   private String href = null;
 
   @JsonProperty("id")
+  @Id
+  @GeneratedValue(generator = "system-uuid")
+  @GenericGenerator(strategy = "uuid", name="system-uuid")
   private String id = null;
 
   @JsonProperty("name")
+  @Column
   private String name = null;
 
   public AgreementRef href(String href) {

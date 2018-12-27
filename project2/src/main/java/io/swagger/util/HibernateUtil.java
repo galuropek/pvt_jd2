413e8a3f7ed85152a9c9e1bcc7c5c2a599f5553b
Галушка Игорь <galuropek@gmail.com>
2018-12-27 13:01:19 +0300

    project add hibernate util

---
+++ project2/src/main/java/io/swagger/util/HibernateUtil.java
@@ -0,0 +1,35 @@
+package io.swagger.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private ThreadLocal<Session> threadLocal = new ThreadLocal<>();
+
+    private SessionFactory sessionFactory;
+
+    private static HibernateUtil hibernateUtil;
+
+    public HibernateUtil() {
+        sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    public static HibernateUtil getInstance() {
+        if (hibernateUtil == null)
+            hibernateUtil = new HibernateUtil();
+        return hibernateUtil;
+    }
+
+    public Session getSession() {
+        Session session = threadLocal.get();
+        if (session == null || !session.isOpen()) {
+            session = sessionFactory.openSession();
+            threadLocal.set(session);
+        }
+        return session;
+    }
+}

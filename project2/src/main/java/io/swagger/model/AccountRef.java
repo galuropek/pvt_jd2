da63fb07470c1233329bbf4a8c38a6d9d2b35d60
Галушка Игорь <galuropek@gmail.com>
2018-12-27 13:40:27 +0300

    add annotations

--- project2/src/main/java/io/swagger/model/AccountRef.java
+++ project2/src/main/java/io/swagger/model/AccountRef.java
@@ -1,11 +1,15 @@
 package io.swagger.model;
 
 import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 
@@ -16,142 +20,153 @@ import javax.validation.constraints.*;
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-12-22T19:45:02.882+03:00")
 
-public class AccountRef   {
-  @JsonProperty("description")
-  private String description = null;
+@Entity
+public class AccountRef {
+    @JsonProperty("description")
+    @Column
+    private String description = null;
 
-  @JsonProperty("href")
-  private String href = null;
+    @JsonProperty("href")
+    @Column
+    private String href = null;
 
-  @JsonProperty("id")
-  private String id = null;
+    @JsonProperty("id")
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(strategy = "uuid", name = "system-uuid")
+    private String id = null;
 
-  @JsonProperty("name")
-  private String name = null;
+    @JsonProperty("name")
+    @Column
+    private String name = null;
 
-  public AccountRef description(String description) {
-    this.description = description;
-    return this;
-  }
-
-  /**
-   * Detailed description of the account
-   * @return description
-  **/
-  @ApiModelProperty(value = "Detailed description of the account")
-
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public AccountRef href(String href) {
-    this.href = href;
-    return this;
-  }
-
-  /**
-   * Reference of the account
-   * @return href
-  **/
-  @ApiModelProperty(value = "Reference of the account")
-
-
-  public String getHref() {
-    return href;
-  }
-
-  public void setHref(String href) {
-    this.href = href;
-  }
-
-  public AccountRef id(String id) {
-    this.id = id;
-    return this;
-  }
-
-  /**
-   * Unique identifier of the account
-   * @return id
-  **/
-  @ApiModelProperty(value = "Unique identifier of the account")
-
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public AccountRef name(String name) {
-    this.name = name;
-    return this;
-  }
-
-  /**
-   * Name of the account
-   * @return name
-  **/
-  @ApiModelProperty(value = "Name of the account")
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+    public AccountRef description(String description) {
+        this.description = description;
+        return this;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    /**
+     * Detailed description of the account
+     *
+     * @return description
+     **/
+    @ApiModelProperty(value = "Detailed description of the account")
+
+
+    public String getDescription() {
+        return description;
     }
-    AccountRef accountRef = (AccountRef) o;
-    return Objects.equals(this.description, accountRef.description) &&
-        Objects.equals(this.href, accountRef.href) &&
-        Objects.equals(this.id, accountRef.id) &&
-        Objects.equals(this.name, accountRef.name);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(description, href, id, name);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class AccountRef {\n");
-    
-    sb.append("    description: ").append(toIndentedString(description)).append("\n");
-    sb.append("    href: ").append(toIndentedString(href)).append("\n");
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    public void setDescription(String description) {
+        this.description = description;
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    public AccountRef href(String href) {
+        this.href = href;
+        return this;
+    }
+
+    /**
+     * Reference of the account
+     *
+     * @return href
+     **/
+    @ApiModelProperty(value = "Reference of the account")
+
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public AccountRef id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Unique identifier of the account
+     *
+     * @return id
+     **/
+    @ApiModelProperty(value = "Unique identifier of the account")
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AccountRef name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * Name of the account
+     *
+     * @return name
+     **/
+    @ApiModelProperty(value = "Name of the account")
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AccountRef accountRef = (AccountRef) o;
+        return Objects.equals(this.description, accountRef.description) &&
+                Objects.equals(this.href, accountRef.href) &&
+                Objects.equals(this.id, accountRef.id) &&
+                Objects.equals(this.name, accountRef.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, href, id, name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class AccountRef {\n");
+
+        sb.append("    description: ").append(toIndentedString(description)).append("\n");
+        sb.append("    href: ").append(toIndentedString(href)).append("\n");
+        sb.append("    id: ").append(toIndentedString(id)).append("\n");
+        sb.append("    name: ").append(toIndentedString(name)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
 

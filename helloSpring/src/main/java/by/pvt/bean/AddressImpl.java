ea16c4ab4ec287a43c3976ce64df03cb9dad5eea
Галушка Игорь <galuropek@gmail.com>
2018-12-28 21:47:00 +0300

    helloSpring

---
+++ helloSpring/src/main/java/by/pvt/bean/AddressImpl.java
@@ -0,0 +1,13 @@
+package by.pvt.bean;
+
+public class AddressImpl implements Address {
+
+
+    public String getStreet() {
+        return "Fish Street";
+    }
+
+    public String getCity() {
+        return "Stambul";
+    }
+}

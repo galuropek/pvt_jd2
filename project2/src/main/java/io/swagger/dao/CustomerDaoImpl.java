fa6c71f626ad0adae87b6cf5a79393a9c92953cf
Галушка Игорь <galuropek@gmail.com>
2019-01-02 21:41:58 +0300

    customerDao

---
+++ project2/src/main/java/io/swagger/dao/CustomerDaoImpl.java
@@ -0,0 +1,29 @@
+package io.swagger.dao;
+
+import io.swagger.model.Customer;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class CustomerDaoImpl<T> extends BaseDaoImpl {
+
+    private static Logger log = Logger.getLogger(Customer.class.getName());
+
+    @Autowired
+    public CustomerDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<T> find() {
+        log.info("Call find()");
+        return openSession().createQuery("from Customer").list();
+    }
+
+    public void save(T item) {
+        openSession().saveOrUpdate(item);
+    }
+}

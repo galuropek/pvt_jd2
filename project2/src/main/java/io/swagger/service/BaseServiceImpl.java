26f63cb00b7e216816334950fef25b230d8ee48e
Галушка Игорь <galuropek@gmail.com>
2019-01-03 16:43:27 +0300

    base service

--- project2/src/main/java/io/swagger/service/BaseServiceImpl.java
+++ project2/src/main/java/io/swagger/service/BaseServiceImpl.java
@@ -1,23 +1,11 @@
 package io.swagger.service;
 
-import io.swagger.dao.BaseDaoImpl;
-import io.swagger.model.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.logging.Logger;
 
-@Service
-public class BaseServiceImpl<T> {
+public abstract class BaseServiceImpl<T> {
 
     private static Logger log = Logger.getLogger(BaseServiceImpl.class.getName());
 
-    @Autowired
-    private BaseDaoImpl<Customer> customerBaseDao;
-
-    public List<T> list() {
-        log.info("BaseServiceImpl:" + customerBaseDao);
-        return (List<T>) customerBaseDao.find();
-    }
+    public abstract List<T> list();
 }

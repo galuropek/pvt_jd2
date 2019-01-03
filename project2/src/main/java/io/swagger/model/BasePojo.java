f3d7d0413a6916673e89fd93a535bcb20eb53605
Галушка Игорь <galuropek@gmail.com>
2019-01-03 16:40:13 +0300

    base pojo for id

---
+++ project2/src/main/java/io/swagger/model/BasePojo.java
@@ -0,0 +1,17 @@
+package io.swagger.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BasePojo {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(strategy = "uuid", name = "system-uuid")
+    private String id;
+}

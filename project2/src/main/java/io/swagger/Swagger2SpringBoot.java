4451e523e28f119b7da59bff9c570fa25434b3ff
Галушка Игорь <galuropek@gmail.com>
2019-01-03 16:45:04 +0300

    main + config

--- project2/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ project2/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -3,15 +3,18 @@ package io.swagger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//если прописываем свои папки
+//если нужно, прописываем свои папки вместо "io.swagger" и т.д.
 @ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override

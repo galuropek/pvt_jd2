b4cded1c2188c62ce81698ba4613b0fa81d21f80
Галушка Игорь <galuropek@gmail.com>
2019-01-03 16:39:02 +0300

    dao

--- project2/src/main/java/io/swagger/dao/CustomerDaoImpl.java
+++ project2/src/main/java/io/swagger/dao/CustomerDaoImpl.java
@@ -3,10 +3,12 @@ package io.swagger.dao;
 import io.swagger.model.Customer;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.logging.Logger;
 
+@Repository
 public class CustomerDaoImpl<T> extends BaseDaoImpl {
 
     private static Logger log = Logger.getLogger(Customer.class.getName());

220edcdaea964b4ec25ca764c8b903b78291a620
Галушка Игорь <galuropek@gmail.com>
2019-01-04 23:55:11 +0300

    helloSpring

--- helloSpring/src/main/java/by/pvt/bean/AddressImpl.java
+++ helloSpring/src/main/java/by/pvt/bean/AddressImpl.java
@@ -1,7 +1,21 @@
 package by.pvt.bean;
 
+import java.util.List;
+import java.util.Map;
+
 public class AddressImpl implements Address {
 
+    private List<String> flats;
+
+    private Map coordinats;
+
+    public Map getCoordinats() {
+        return coordinats;
+    }
+
+    public void setCoordinats(Map coordinats) {
+        this.coordinats = coordinats;
+    }
 
     public String getStreet() {
         return "Fish Street";
@@ -9,5 +23,13 @@ public class AddressImpl implements Address {
 
     public String getCity() {
         return "Stambul";
+    }
+
+    public List<String> getFlats() {
+        return flats;
+    }
+
+    public void setFlats(List<String> flats) {
+        this.flats = flats;
     }
 }

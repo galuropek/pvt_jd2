a5f1c6fe2341388c50e5a4865006896d942e8de8
Галушка Игорь <galuropek@gmail.com>
2019-01-12 19:10:00 +0300

    helloSpring

---
+++ helloSpring/src/main/java/by/pvt/pojo/Person.java
@@ -0,0 +1,51 @@
+package by.pvt.pojo;
+
+public class Person {
+
+    private int id;
+    private String name;
+    private String secondName;
+    Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}

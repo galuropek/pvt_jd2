d69ac1b2b1553488d79526ba30d89b9a684166a2
Галушка Игорь <galuropek@gmail.com>
2019-01-17 21:18:39 +0300

    add update method

--- AlarmManagement/src/test/java/io/swagger/dao/AlarmDaoImplTest.java
+++ AlarmManagement/src/test/java/io/swagger/dao/AlarmDaoImplTest.java
@@ -3,18 +3,22 @@ package io.swagger.dao;
 import io.swagger.configuration.AlarmInit;
 import io.swagger.configuration.HibernateXMLConfTest;
 import io.swagger.model.Alarm;
+import io.swagger.model.AlarmedObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
@@ -27,6 +31,12 @@ import static org.junit.Assert.*;
 @Transactional
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AlarmDaoImplTest {
+
+    @MockBean
+    com.fasterxml.jackson.databind.ObjectMapper objectMapper;
+
+    @MockBean
+    HttpServletRequest httpServletRequest;
 
     @Resource
     AlarmDaoImpl alarmDao;
@@ -105,22 +115,74 @@ public class AlarmDaoImplTest {
 
     @Test
     public void step4_update() {
-        //do something
+        Alarm alarm5 = AlarmInit.init("5");
+        alarmDao.save(alarm5);
+        assertTrue(alarm5.getId() == 5L);
+        assertEquals(alarm5.getAckUserId(), "AckUserId5");
+        System.out.println("---> Print Alarm5:\n" + alarm5);
 
+        assertEquals(alarm5.getType(), "Type5");
+        assertEquals(alarm5.getBaseType(), "BaseType5");
+        assertEquals(alarm5.getAlarmType(), "AlarmType5");
+        assertEquals(alarm5.getClearSystemId(), "ClearSystemId5");
+
+        assertEquals(alarm5.getComments().get(0).getComment(), "Comment1");
+        assertNotNull(alarm5.getComments().get(0).getSystemId());
+
+        assertEquals(alarm5.getCrossedThresholdInformation().getDirection(), "Direction5");
+        assertNotNull(alarm5.getCrossedThresholdInformation().getThresholdId());
+
+
+        assertEquals(alarm5.getParentAlarm().get(0).getHref(), "Href1");
+        assertNotNull(alarm5.getParentAlarm().get(0).getId());
+
+        assertEquals(alarm5.getCorrelatedAlarm().get(0).getHref(), "Href1");
+        assertNotNull(alarm5.getCorrelatedAlarm().get(0).getId());
+
+        assertEquals(alarm5.getAffectedService().get(0).getHref(), "Href1");
+        assertNotNull(alarm5.getAffectedService().get(0).getId());
+
+        assertEquals(alarm5.getAlarmedObject().getHref(), "Href5");
+        assertNotNull(alarm5.getAlarmedObject().getId());
+
+//        Alarm update = AlarmInit.init("7");
+//        update.setId(alarm5.getId());
+//        update.setAlarmedObject(alarm5.getAlarmedObject());
+//        update.setAffectedService(alarm5.getAffectedService());
+//        update.setCorrelatedAlarm(alarm5.getCorrelatedAlarm());
+//        update.setParentAlarm(alarm5.getParentAlarm());
+//        update.setCrossedThresholdInformation(alarm5.getCrossedThresholdInformation());
+//        update.setComments(alarm5.getComments());
+//        System.out.println(update);
+//        alarm5.setId(null);
+//        System.out.println(alarm5);
+//        alarmDao.save(update);
+//        System.out.println(alarmDao.get(5L));
+
+        Alarm alarmUpdate = new Alarm()
+                .id(alarm5.getId())
+                .type("TypeUpdate")
+                .alarmedObject(new AlarmedObject()
+                        .href("hrefUpdate"));
+        BeanUtils.copyProperties(alarmUpdate, alarm5);
+        alarmDao.update(alarm5);
+        System.out.println(alarm5);
+        Alarm alarm = alarmDao.get(alarm5.getId());
+        System.out.println(alarm);
     }
 
     @Test
     public void step5_delete() {
-        Alarm alarm6 = AlarmInit.init("5");
+        Alarm alarm6 = AlarmInit.init("6");
         alarmDao.save(alarm6);
-        assertTrue(alarm6.getId() == 5L);
+        assertTrue(alarm6.getId() == 6L);
 
-        Alarm getAlarm = alarmDao.get(5L);
+        Alarm getAlarm = alarmDao.get(6L);
         assertEquals(alarm6, getAlarm);
 
         alarmDao.delete(getAlarm);
 
-        Alarm getAfterDelete = alarmDao.get(5L);
+        Alarm getAfterDelete = alarmDao.get(6L);
         assertNull(getAfterDelete);
     }
 

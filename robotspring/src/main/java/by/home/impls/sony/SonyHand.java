41667c407d3ba2d434118b8b9d7bc11fbea9c26c
Галушка Игорь <galuropek@gmail.com>
2019-01-22 18:54:51 +0300

    New example spring: implements interface Hand

---
+++ robotspring/src/main/java/by/home/impls/sony/SonyHand.java
@@ -0,0 +1,11 @@
+package by.home.impls.sony;
+
+import by.home.interfaces.Hand;
+
+public class SonyHand implements Hand {
+	
+	public void catchSomething(){
+		System.out.println("Catched from Sony!!");
+	}
+
+}

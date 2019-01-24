4bc7d311498b88b548a00ecd987493d0e14cd625
Галушка Игорь <galuropek@gmail.com>
2019-01-24 14:15:04 +0300

    Bean extension: create abstract class BaseModel

---
+++ robotspring/src/main/java/by/home/impls/robot/BaseModel.java
@@ -0,0 +1,47 @@
+package by.home.impls.robot;
+
+import by.home.interfaces.Hand;
+import by.home.interfaces.Head;
+import by.home.interfaces.Leg;
+import by.home.interfaces.Robot;
+
+public abstract class BaseModel implements Robot {
+
+    private Hand hand;
+    private Leg leg;
+    private Head head;
+
+    public BaseModel() {
+        System.out.println(this + " - BaseModel constructor");
+    }
+
+    public BaseModel(Hand hand, Leg leg, Head head) {
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+}

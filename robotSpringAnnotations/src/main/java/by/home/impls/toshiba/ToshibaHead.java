fe883db9c293fa2ef415417784f1634aa7f9c78e
Галушка Игорь <galuropek@gmail.com>
2019-01-26 19:21:02 +0300

    Component scan: using @Component

--- robotSpringAnnotations/src/main/java/by/home/impls/toshiba/ToshibaHead.java
+++ robotSpringAnnotations/src/main/java/by/home/impls/toshiba/ToshibaHead.java
@@ -1,7 +1,9 @@
 package by.home.impls.toshiba;
 
 import by.home.interfaces.Head;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ToshibaHead implements Head {
 	
 	public void calc(){

164af00e822647d88dd1f539481ac5bf3722986a
Галушка Игорь <galuropek@gmail.com>
2019-01-26 19:20:46 +0300

    Component scan: using @Component

--- robotSpringAnnotations/src/main/java/by/home/impls/toshiba/ToshibaHand.java
+++ robotSpringAnnotations/src/main/java/by/home/impls/toshiba/ToshibaHand.java
@@ -1,7 +1,9 @@
 package by.home.impls.toshiba;
 
 import by.home.interfaces.Hand;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ToshibaHand implements Hand {
 	
 	public void catchSomething(){

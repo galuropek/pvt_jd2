5d74e7df6f081f6587e87cb82e835fd50bb63735
Галушка Игорь <galuropek@gmail.com>
2019-01-27 22:57:28 +0300

    Edit method alarmGet()

--- AlarmManagement/src/main/java/io/swagger/api/AlarmApi.java
+++ AlarmManagement/src/main/java/io/swagger/api/AlarmApi.java
@@ -8,18 +8,13 @@ package io.swagger.api;
 import io.swagger.model.Alarm;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
 import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-01-06T21:03:59.754+03:00")
 
@@ -63,7 +58,7 @@ public interface AlarmApi {
         produces = { "application/json" }, 
 //        consumes = { "application/json" },
         method = RequestMethod.GET)
-    ResponseEntity<Alarm> alarmGet(@ApiParam(value = "",required=true) @PathVariable("alarmId") String alarmId,@ApiParam(value = "") @Valid @RequestParam(value = "fields", required = false) String fields);
+    ResponseEntity<Object> alarmGet(@ApiParam(value = "",required=true) @PathVariable("alarmId") String alarmId,@ApiParam(value = "") @Valid @RequestParam(value = "fields", required = false) String fields);
 
 
     @ApiOperation(value = "alarmPatch", nickname = "alarmPatch", notes = "", response = Alarm.class, tags={ "alarm", })

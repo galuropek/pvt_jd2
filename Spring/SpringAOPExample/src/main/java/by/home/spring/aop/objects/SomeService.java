1489fc5f836212780a0e429e95fada51b8e22dbe
Галушка Игорь <galuropek@gmail.com>
2019-01-28 17:44:33 +0300

    Create SomeService class with methods: getValue(), getDoubleValue().

---
+++ Spring/SpringAOPExample/src/main/java/by/home/spring/aop/objects/SomeService.java
@@ -0,0 +1,12 @@
+package by.home.spring.aop.objects;
+
+public class SomeService {
+
+    public int getValue() {
+        return 5;
+    }
+
+    public double getDoubleValue() {
+        return 5.6;
+    }
+}

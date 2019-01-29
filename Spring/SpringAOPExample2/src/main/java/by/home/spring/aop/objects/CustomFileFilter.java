0219ead77ece9aaa13795d3a5a471dd75264ceba
Галушка Игорь <galuropek@gmail.com>
2019-01-29 14:44:27 +0300

    AOPExample2: added @Lazy annotation

--- Spring/SpringAOPExample2/src/main/java/by/home/spring/aop/objects/CustomFileFilter.java
+++ Spring/SpringAOPExample2/src/main/java/by/home/spring/aop/objects/CustomFileFilter.java
@@ -1,14 +1,19 @@
 package by.home.spring.aop.objects;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FilenameFilter;
 
 @Component
+@Lazy
 public class CustomFileFilter implements FilenameFilter {
 
     private String extension;
+
+//    public CustomFileFilter() {
+//    }
 
     public CustomFileFilter(String extension) {
         this.extension = extension;

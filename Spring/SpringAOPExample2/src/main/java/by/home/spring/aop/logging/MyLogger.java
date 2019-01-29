3af25cd2426b0cc9af26e3c16712e46309474647
Галушка Игорь <galuropek@gmail.com>
2019-01-29 15:20:32 +0300

    AOPExample2: added printStatistics() method

--- Spring/SpringAOPExample2/src/main/java/by/home/spring/aop/logging/MyLogger.java
+++ Spring/SpringAOPExample2/src/main/java/by/home/spring/aop/logging/MyLogger.java
@@ -3,6 +3,9 @@ package by.home.spring.aop.logging;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.Set;
+
 @Component
 public class MyLogger {
 
@@ -10,6 +13,11 @@ public class MyLogger {
         long start = System.currentTimeMillis();
         System.out.println("method begin: " + joinPoint.getSignature().toShortString());
         Object output = null;
+
+        for (Object arg : joinPoint.getArgs()) {
+            System.out.println("Param: " + arg);
+        }
+
         try {
             output = joinPoint.proceed();
         } catch (Throwable e) {
@@ -20,5 +28,24 @@ public class MyLogger {
         System.out.println("method end: " + joinPoint.getSignature().toShortString() + ", time=" + time + "ms");
 
         return output;
+    }
+
+    public void printStatistics(Object obj) {
+
+        System.out.println("Print info begin >>");
+
+        if (obj instanceof Set) {
+            Set set = (Set) obj;
+            for (Object object : set) {
+                System.out.println(object);
+            }
+        } else if (obj instanceof Map) {
+            Map map = (Map) obj;
+            for (Object object : map.keySet()) {
+                System.out.println(object + "=" + map.get(object));
+            }
+        }
+
+        System.out.println("Print info end <<\n");
     }
 }

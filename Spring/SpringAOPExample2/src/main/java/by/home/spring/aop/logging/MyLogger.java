cbc5b623ef7dede9b8df19aa1f393d7ff05ea31f
Галушка Игорь <galuropek@gmail.com>
2019-01-29 13:49:24 +0300

    AOPExample2: created MyLogger class with watchTime() method

---
+++ Spring/SpringAOPExample2/src/main/java/by/home/spring/aop/logging/MyLogger.java
@@ -0,0 +1,22 @@
+package by.home.spring.aop.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class MyLogger {
+
+    public Object watchTime(ProceedingJoinPoint joinPoint) {
+        long start = System.currentTimeMillis();
+        System.out.println("method begin: " + joinPoint.getSignature().toShortString());
+        Object output = null;
+        try {
+            output = joinPoint.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        long time = System.currentTimeMillis() - start;
+        System.out.println("method end: " + joinPoint.getSignature().toShortString() + ", time=" + time + "ms");
+
+        return output;
+    }
+}

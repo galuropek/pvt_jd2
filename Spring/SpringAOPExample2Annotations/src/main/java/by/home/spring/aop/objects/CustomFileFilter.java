4b27b4c154a3921ea940ec82d73f721d971d430c
Галушка Игорь <galuropek@gmail.com>
2019-01-30 23:19:17 +0300

    AOPExample2: CustomFileFilter class for annotations example

---
+++ Spring/SpringAOPExample2Annotations/src/main/java/by/home/spring/aop/objects/CustomFileFilter.java
@@ -0,0 +1,21 @@
+package by.home.spring.aop.objects;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class CustomFileFilter implements FilenameFilter {
+
+    private String extension;
+
+//    public CustomFileFilter() {
+//    }
+
+    public CustomFileFilter(String extension) {
+        this.extension = extension;
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.toUpperCase().endsWith("." + extension.toUpperCase());
+    }
+}

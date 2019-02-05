2751c543e8c26ef602c58e7e9fa01cea63eb1db5
Галушка Игорь <galuropek@gmail.com>
2019-02-05 19:40:56 +0300

    SpringDBSQLiteExample: implement method get List by name

--- Spring/SpringDBSQLite/src/main/java/by/home/springsqlitedb/main/Start.java
+++ Spring/SpringDBSQLite/src/main/java/by/home/springsqlitedb/main/Start.java
@@ -9,12 +9,20 @@ public class Start {
 
     public static void main(String[] args) {
         MP3 mp3 = new MP3();
-        mp3.setName("Jumpsuit");
-        mp3.setAuthor("Twenty One Pilots");
+        mp3.setName("Bohemian Rhapsody");
+        mp3.setAuthor("Queen");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         MP3DaoImpl mp3Dao = (MP3DaoImpl) context.getBean("mp3Dao");
 
         mp3Dao.insert(mp3);
+//        mp3Dao.delete(mp3);
+        System.out.println(mp3Dao.getMP3ById(22));
+        System.out.println("--------------------------------------------");
+        System.out.println(mp3Dao.getMP3ListByName("hello"));
+        System.out.println("--------------------------------------------");
+        System.out.println(mp3Dao.getMp3ListByAuthor("linkin"));
+        System.out.println("--------------------------------------------");
+        System.out.println("Count mp3: " + mp3Dao.getCount());
     }
 }

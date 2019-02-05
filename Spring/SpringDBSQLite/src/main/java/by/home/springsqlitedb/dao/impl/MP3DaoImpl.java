66a16c7302093c2868a12ebd2cc4bcb0d008dea8
Галушка Игорь <galuropek@gmail.com>
2019-02-05 15:19:05 +0300

    SpringDBSQLiteExample: create mp3DaoImpl, implement insert method

---
+++ Spring/SpringDBSQLite/src/main/java/by/home/springsqlitedb/dao/impl/MP3DaoImpl.java
@@ -0,0 +1,43 @@
+package by.home.springsqlitedb.dao.impl;
+
+import by.home.springsqlitedb.dao.MP3Dao;
+import by.home.springsqlitedb.model.MP3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Component("mp3Dao")
+public class MP3DaoImpl implements MP3Dao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void insert(MP3 mp3) {
+        String sql = "insert into mp3 (name, author) VALUES (?, ?)";
+        jdbcTemplate.update(sql, mp3.getName(), mp3.getAuthor());
+        System.out.println("insert(): \n" + mp3);
+    }
+
+    public void delete(MP3 mp3) {
+
+    }
+
+    public MP3 getMP3byId(int id) {
+        return null;
+    }
+
+    public List<MP3> getMP3ListByName(String name) {
+        return null;
+    }
+
+    public List<MP3> getMp3ListByAuthor(String author) {
+        return null;
+    }
+}

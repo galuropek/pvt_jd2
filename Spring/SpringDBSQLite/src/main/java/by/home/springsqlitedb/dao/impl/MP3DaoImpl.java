2a77d090072121c9765afb1af26069095aedb624
Галушка Игорь <galuropek@gmail.com>
2019-02-05 16:52:06 +0300

    SpringDBSQLiteExample: create insert List method and edit delete() method

--- Spring/SpringDBSQLite/src/main/java/by/home/springsqlitedb/dao/impl/MP3DaoImpl.java
+++ Spring/SpringDBSQLite/src/main/java/by/home/springsqlitedb/dao/impl/MP3DaoImpl.java
@@ -14,6 +14,8 @@ public class MP3DaoImpl implements MP3Dao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private Class<MP3> aClass = MP3.class;
+
     @Autowired
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -25,9 +27,15 @@ public class MP3DaoImpl implements MP3Dao {
         System.out.println("insert(): \n" + mp3);
     }
 
+    public void insert(List<MP3> list) {
+        for (MP3 mp3 : list) {
+            insert(mp3);
+        }
+    }
+
     public void delete(MP3 mp3) {
         if (mp3.getId() != null) {
-            deleteById(mp3);
+            delete(mp3.getId());
             System.out.println("deleteById():\n" + mp3);
         } else {
             if (mp3.getName() != null && mp3.getAuthor() != null) {
@@ -40,21 +48,22 @@ public class MP3DaoImpl implements MP3Dao {
 
     }
 
-    private void deleteById(MP3 mp3) {
-        String sql = "delete from mp3 where id=" + mp3.getId();
-        jdbcTemplate.execute(sql);
+    private void delete(Integer id) {
+        String sql = "delete from mp3 where id=?";
+        jdbcTemplate.update(sql, id);
     }
 
     private void deleteByFields(MP3 mp3) {
         String sql =
-                "delete from mp3 where name='"
-                        + mp3.getName()
-                        + "' and author='"
-                        + mp3.getAuthor() + "'";
+                "delete from mp3 where name='" + mp3.getName()
+                        + "' and author='" + mp3.getAuthor() + "'";
         jdbcTemplate.execute(sql);
     }
 
+    //in process...
     public MP3 getMP3byId(int id) {
+//        String sql = "select * from mp3 where id=" + id;
+//        return jdbcTemplate.queryForObject(sql, aClass);
         return null;
     }
 
